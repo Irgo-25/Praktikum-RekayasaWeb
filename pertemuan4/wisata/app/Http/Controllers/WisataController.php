0bd0d0d79e4d70938fd4873daa8bfdc4aea7659e
Irgo-25 <irgosg@gmail.com>
2024-11-09 23:35:17 +0700

    Praktikum 6

---
+++ pertemuan4/wisata/app/Http/Controllers/WisataController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Wisata;
+use Illuminate\Http\Request;
+
+class WisataController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function showAll()
+    {
+        return response()->json(Wisata::all());
+    }
+
+    public function showOne($id)
+    {
+        return response()->json(Wisata::find($id));
+    }
+
+    public function create(Request $request){
+        $wisata = Wisata::create($request->all());
+        return response()->json($wisata, 201);
+    }
+
+    public function update($id, Request $request){
+        $wisata = Wisata::findOrFail($id);
+        $wisata->update($request->all());
+        return response()->json($wisata, 200);
+    }
+
+    public function delete($id){
+        Wisata::findOrFail($id)->delete();
+        return response('Delete Berhasil', 200);
+    }
+}
